0ad2ea45fa2f73c19e4e98b7318317e53e5bd542
Neel Smith <nsmith@Technites.local>
2021-03-04 05:01:29 -0500

    Add notes for uninflected

--- src/datainstallers.jl
+++ src/datainstallers.jl
@@ -1,4 +1,10 @@
 # Build FST from tables
 
-function installfiniteverbdata()
+function installfiniteverbstems()
 end
+
+function installindeclinablestems()
+end
+#=
+<u>latcommon\.indecln7426</u><u>ls\.n7426</u>centum<indecl><indeclnum>
+=#
