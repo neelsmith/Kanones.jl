b58e95d60c071fecf9850d5e127a795713f28c68
Neel Smith <dnsmith.neel@gmail.com>
2021-03-08 23:13:47 -0500

    Complete compounding table

--- src/unicode.jl
+++ src/unicode.jl
@@ -32,7 +32,6 @@ function compoundsdict()
         'ᾶ' => "α",
         'ᾷ' => "α<isub>",
 
-
         'ᾀ' => "α<sm><isub>",
         'ᾁ' => "α<ro><isub>",
         'ᾂ' => "α<sm><isub>",
@@ -43,11 +42,75 @@ function compoundsdict()
         'ᾆ' => "α<ro><isub>",
         
         # Long vowel series:
-
         # hta series like alpha
-        # iota series like alpha
-        # upsilon series like alpha
+        'ἠ' => "η<sm>",
+        'ἡ' => "η<ro>",
+        'ἢ' => "η<sm>",
+        'ἣ' => "η<ro>",
+        'ἤ' => "η<sm>",
+        'ἥ' => "η<ro>",
+        'ἦ' => "η<sm>",
+        'ἧ' => "η<ro>",
+
+        'ῂ' => "η<isub>",
+        'ῃ' => "η<isub>",
+        'ῄ' => "η<isub>",
+        'ῆ' => "η",
+        'ῇ' => "η<isub>",
+
+        'ᾐ' => "η<sm><isub>",
+        'ᾑ' => "η<ro><isub>",
+        'ᾒ' => "η<sm><isub>",
+        'ᾓ' => "η<ro><isub>",
+        'ᾕ' => "η<sm><isub>",
+        'ᾕ' => "η<ro><isub>",
+        'ᾖ' => "η<sm><isub>",
+        'ᾗ' => "η<ro><isub>",
+
+        # iota series
+        'ἰ' => "ι<sm>",
+        'ἱ' => "ι<ro>",
+        'ἲ' => "ι<sm>",
+        'ἳ' => "ι<ro>",
+        'ἴ' => "ι<sm>",
+        'ἵ' => "ι<ro>",
+        'ἶ' => "ι<sm>",
+        'ἷ' => "ι<ro>",
+
+        # upsilon series like iota
+        'ὐ' => "υ<sm>",
+        'ὑ' => "υ<ro>",
+        'ὒ' => "υ<sm>",
+        'ὕ' => "υ<ro>",
+        'ὔ' => "υ<sm>",
+        'ὕ' => "υ<ro>",
+        'ὖ' => "υ<sm>",
+        'ὗ' => "υ<ro>",
+
         # omega series like alpha
+        'ὠ' => "ω<sm>",
+        'ὡ' => "ω<ro>",
+        'ὢ' => "ω<sm>",
+        'ὣ' => "ω<ro>",
+        'ὤ' => "ω<sm>",
+        'ὥ' => "ω<ro>",
+        'ὦ' => "ω<sm>",
+        'ὧ' => "ω<ro>",
+
+        'ῲ' => "ω<isub>",
+        'ῳ' => "ω<isub>",
+        'ῴ' => "ω<isub>",
+        'ῶ' => "ω",
+        'ῷ' => "ω<isub>",
+
+        'ᾠ' => "ω<sm><isub>",
+        'ᾡ' => "ω<ro><isub>",
+        'ᾢ' => "ω<sm><isub>",
+        'ᾣ' => "ω<ro><isub>",
+        'ᾤ' => "ω<sm><isub>",
+        'ᾤ' => "ω<ro><isub>",
+        'ᾦ' => "ω<sm><isub>",
+        'ᾧ' => "ω<ro><isub>",
 
         # Short vowel series:
         'ἐ' => "ε<sm>",
