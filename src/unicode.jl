423883ac63542e50aab4038c8a5043b1f7f62cd2
Neel Smith <dnsmith.neel@gmail.com>
2021-03-08 20:19:24 -0500

    Start on big compounds dictionary

--- src/unicode.jl
+++ src/unicode.jl
@@ -17,7 +17,36 @@ end
 
 function compoundsdict()
     d = Dict(
-        'ἀ' => "α<sm>"
+        'ἀ' => "α<sm>",
+        'ἁ' => "α<ro>",
+        'ἂ' => "α<sm>",
+        'ἃ' => "α<ro>",
+        'ἄ' => "α<sm>",
+        'ἅ' => "α<ro>",
+        'ἆ' => "α<sm>",
+        'ἇ' => "α<ro>",
+
+        'ᾲ' => "α<isub>",
+        'ᾳ' => "α<isub>",
+        'ᾴ' => "α<isub>",
+        'ᾶ' => "α",
+        'ᾷ' => "α<isub>",
+        
+        # Long vowel series:
+
+        # hta series like alpha
+        # iota series like alpha
+        # upsilon series like alpha
+        # omega series like alpha
+
+        # Short vowel series:
+        'ἐ' => "ε<sm>",
+        'ἑ' => "ε<ro>",
+        'ἒ' => "ε<sm>",
+        'ἓ' => "ε<ro>",
+        'ἔ' => "ε<sm>",
+        'ἕ' => "ε<ro>",
+        # omicron series like epislon    
     )
-    d
+    
 end
