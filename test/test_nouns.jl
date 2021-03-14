67add2a0ebc567f0a91b936504d681e98b78f5c5
Neel Smith <dnsmith.neel@gmail.com>
2021-03-14 12:20:45 -0400

    Define tests for nouns

--- test/test_nouns.jl
+++ test/test_nouns.jl
@@ -1,6 +1,3 @@
-
-
-
 @testset "Read rules for noun tokens from delimited text" begin
     cex = "nouninfl.h_hs2|h_hs|ης|feminine|genitive|singular|"
     nounparser = FstBuilder.NounParser("nouns")
@@ -9,4 +6,32 @@
     #expectedrule = Kanones.RuleUrn("litgreek.indeclinable2")
     #@test rulerow.ruleid.collection == "litgreek"
     #@test rulerow.ruleid.objectid == "indeclinable2"
+end
+
+
+@testset "Write fst rules for noun tokens" begin
+end
+
+
+@testset "Read stems for noun tokens from delimited text"  begin    
+end
+
+@testset "Write fst lexicon for noun stems" begin
+end
+
+
+
+@testset "Build parser and parse noun forms" begin
+    #=
+    d = tempdir()
+    repo = dirname(pwd())
+
+    kd = Kanones.Dataset(repo * "/datasets/synoptic/")
+    fst =  repo * "/fst/"
+    FstBuilder.buildparser(kd,fst, d * "/testcompile/")
+    
+    parser = d * "/testcompile/greek.a"
+    =#
+    #analyzed = parsetoken(parser, "και")
+    #println(analyzed)
 end
