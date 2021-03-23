83a0e5a1029439ba46ed5a4bdb22fda81034f2d4
Neel Smith <dnsmith.neel@gmail.com>
2021-03-23 10:38:12 -0400

    Test nouns data structure from delimited text.

--- test/nouns/test_nounsdata.jl
+++ test/nouns/test_nounsdata.jl
@@ -2,17 +2,24 @@
     cex = "nouninfl.h_hs2|h_hs|ης|feminine|genitive|singular|recessive"
     nounparser = Kanones.NounParser("nouns")
     rulerow = Kanones.readrulerow(nounparser, cex)
-    
+    @test rulerow.ruleid |> abbreviation == RuleUrn("nouninfl.h_hs2") |> abbreviation
     @test rulerow.inflectionclass == "h_hs"
-    #expectedrule = Kanones.RuleUrn("litgreek.indeclinable2")
-    #@test rulerow.ruleid.collection == "litgreek"
-    #@test rulerow.ruleid.objectid == "indeclinable2"
+    @test rulerow.ending == "ης"
+    @test rulerow.ngender == "feminine"
+    @test rulerow.ncase == "genitive"
+    @test rulerow.nnumber == "singular"
 end
 
 @testset "Read stems for noun tokens from delimited text"  begin    
-    cex = "nounstems.n22502|lexent.n22502|γνωμ|masculine|h_hs|recessive|"
+    cex = "nounstems.n22502|lexent.n22502|γνωμ|feminine|h_hs|recessive|"
     nounparser = Kanones.NounParser("nouns")
     stemrow = Kanones.readstemrow(nounparser, cex)
+    @test  stemrow.stemid |> abbreviation == StemUrn("nounstems.n22502") |> abbreviation
+    @test stemrow.lexid |> abbreviation == LexemeUrn("lexent.n22502") |> abbreviation
+    @test stemrow.form == "γνωμ"
+    @test stemrow.gender == "feminine"
+    @test stemrow.inflectionclass == "h_hs"
+    @test stemrow.accentpersistence == "recessive"
 end
 
 
--- test/nouns/test_nounsfst.jl
+++ test/nouns/test_nounsfst.jl
@@ -1,16 +1,18 @@
-@testset "Write fst rules for noun tokens" begin
+@testset "Format fst rules for noun tokens mu" begin
     cex = "nouninfl.h_hs2|h_hs|ης|feminine|genitive|singular|recessive"
     nounparser = Kanones.NounParser("nouns")
     rulerow = Kanones.readrulerow(nounparser, cex)
-    
-    FstBuilder.fst(rulerow)
+    # Note that "." and "_" must be protected in FST string values, but not in symbols.
+    expected = "<h_hs><noun>ης<feminine><genitive><singular><u>nouninfl\\.h\\_hs2</u>"
+    @test FstBuilder.fst(rulerow) == expected
 end
 
-@testset "Write fst lexicon for noun stems" begin
+@testset "Format fst lexicon for noun stems" begin
     cex = "nounstems.n22502|lexent.n22502|γνωμ|masculine|h_hs|recessive|"
     nounparser = Kanones.NounParser("nouns")
     stemrow = Kanones.readstemrow(nounparser, cex)
-
-    FstBuilder.fst(stemrow)
+    # Note that "." and "_" must be protected in FST string values, but not in symbols.
+    expected = "<u>nounstems\\.n22502</u><u>lexent\\.n22502</u>γνωμ<noun><masculine><h_hs>"
+    @test FstBuilder.fst(stemrow) == expected
 end
 
