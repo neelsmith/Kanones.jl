6d9bb8e59229e7cf38338aadbb288db4a66f1aa0
Neel Smith <dnsmith.neel@gmail.com>
2021-04-08 15:06:38 -0400

    Work on core uninflected list

--- src/utils.jl
+++ src/utils.jl
@@ -11,8 +11,8 @@ function sortstemfile(f; lsjcolumn = 2)
     for i = 2:size(dataset,1)
         ln = dataset[i,:]
         if startswith(ln[lsjcolumn], "lsj.n")
-
-            intkey = parse(Int64, replace(ln[lsjcolumn], "lsj.n" => ""))
+            trimtrailingalpha = replace(ln[lsjcolumn], r"[a-z]$" => "")
+            intkey = parse(Int64, replace(trimtrailingalpha, "lsj.n" => ""))
             push!(sortable, (intkey, ln))
         else
             push!(sortable, (40000,ln))
@@ -21,6 +21,6 @@ function sortstemfile(f; lsjcolumn = 2)
     sorted = map(pr -> join(pr[lsjcolumn], "|"),  sort(sortable))
     contents = string(join(dataset[1,:],"|"), "\n\n", join(sorted, "\n"))
     open(f,"w") do io
-        write(io,contents)
+       write(io,contents)
     end
 end
