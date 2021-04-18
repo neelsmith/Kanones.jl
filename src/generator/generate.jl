7741eaf85cff6c13427020b937664da71cb2e76f
Neel Smith <nsmith@Technites.local>
2021-04-18 06:59:45 -0400

    Set up generator pipeline

--- src/generator/generate.jl
+++ src/generator/generate.jl
@@ -4,29 +4,16 @@
 
 $(SIGNATURES)
 """
-function generate(analysis::Analysis)
-    # is token == stem.form + rule.ending ?
-    #
-    # treatment depends on PoS.
-    # So get that from column 1 of form.
-    #=
-    struct Analysis
-        token::AbstractString
-        lexeme::LexemeUrn
-        form::FormUrn
-        rule::RuleUrn
-        stem::StemUrn
+function generate(analysis::Analysis; ortho::T = literaryGreek()) where {T <: GreekOrthography}
+    # Find "part of speech" value from formurn:
+    formcolumns = split(analysis.form.objectid,"")
+    pos = parse(Int64, formcolumns[1])
+    
+    if pos == NOUN
+        println("GEN NOUN")
+    elseif posint == UNINFLECTED
+        pritnln("GEN UNINFL")
+    else
+        println("NOT YET IMPLEMENTED")
     end
-    =#
-    formcolumns = split(accpl.form.objectid,"")
-    dict = valuedict(pospairs)
-    # Switch on this value:
-    pos = dict[parse(Int64, formcolumns[1])]
-    # Defined so far:
-    #=
-const UNANALYZED = 0
-const UNINFLECTED = 1
-const NOUN = 2
-const FINITEVERB = 3
-    =#
 end
