572c747847d8cef5ddafa07e66eda6eb84050a2a
Neel Smith <nsmith@Technites.local>
2021-04-20 03:01:56 -0400

    Organize decline function

--- src/morphology/forapps.jl
+++ src/morphology/forapps.jl
@@ -8,34 +8,40 @@ function decline(lex::LexemeUrn, kd::Kanones.Dataset; withvocative::Bool = false
     nounstems = filter(s -> typeof(s) == NounStem, stems)
     stemmatches = filter(s -> s.lexid == lex, nounstems)
     
-    # Make sure stemmatches is not empty first!
-    gender = stemmatches[1].gender
-    # Find gender and construct form ids:
-    genderdict = labeldict(genderpairs)
-    #morphchars = split(stemmatches[1].gender,"")
-    genderint = genderdict[stemmatches[1].gender]
-    sg = [
-        FormUrn("morphform.201000$(genderint)100"),
-        FormUrn("morphform.201000$(genderint)200"),
-        FormUrn("morphform.201000$(genderint)300"),
-        FormUrn("morphform.201000$(genderint)400")
-    ]
-    pl = [
-        FormUrn("morphform.202000$(genderint)100"),
-        FormUrn("morphform.202000$(genderint)200"),
-        FormUrn("morphform.202000$(genderint)300"),
-        FormUrn("morphform.202000$(genderint)400")
-    ]
+    # Make sure stemmatches is not empty first:
+    if isempty(stemmatches)
+        @warn "No matches in dataset for lexeme $lex"
+        nothing
 
-    rules = rulesarray(kd)
-    nounrules = filter(r -> typeof(r) == NounRule, rules)
+    else
+        gender = stemmatches[1].gender
+        # Find gender and construct form ids:
+        genderdict = labeldict(genderpairs)
+        #morphchars = split(stemmatches[1].gender,"")
+        genderint = genderdict[stemmatches[1].gender]
+        sg = [
+            FormUrn("morphform.201000$(genderint)100"),
+            FormUrn("morphform.201000$(genderint)200"),
+            FormUrn("morphform.201000$(genderint)300"),
+            FormUrn("morphform.201000$(genderint)400")
+        ]
+        pl = [
+            FormUrn("morphform.202000$(genderint)100"),
+            FormUrn("morphform.202000$(genderint)200"),
+            FormUrn("morphform.202000$(genderint)300"),
+            FormUrn("morphform.202000$(genderint)400")
+        ]
 
-    forms = []
-    for s in sg
-        println("generate $s")
+        rules = rulesarray(kd)
+        nounrules = filter(r -> typeof(r) == NounRule, rules)
+
+        forms = []
+        for s in sg
+            println("generate $s")
+        end
+        for p in pl
+            println("generate $p")
+        end
+        forms
     end
-    for p in pl
-        println("generate $p")
-    end
-    forms
 end
