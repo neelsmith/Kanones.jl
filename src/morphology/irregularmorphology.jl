08ced191e1ad21f4f17cab350732061794491d0b
Neel Smith <nsmith@Technites.local>
2021-04-25 09:49:53 -0400

    Defin place hodler for function

--- src/morphology/irregularmorphology.jl
+++ src/morphology/irregularmorphology.jl
@@ -2,4 +2,6 @@
 function irregularabbrurn(fstdata::AbstractString)
     # Parse fstdata
     # based on irregular type, construct urn for form
+    irregrulere = r"<([^<]+)><([^<]+)>(.+)"  
+    
 end
