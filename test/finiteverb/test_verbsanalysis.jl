5892dd660cb0ed743edf9dc6c00d0429adc4d27b
Neel Smith <dnsmith.neel@gmail.com>
2021-08-02 15:28:12 -0400

    Test a ton of verb forms

--- test/finiteverb/test_verbsanalysis.jl
+++ test/finiteverb/test_verbsanalysis.jl
@@ -12,4 +12,72 @@
     @test isa(analyzed[1], Analysis)
     @test analyzed[1].token == "κελευσει"
 
+
+    analyzed = parsetoken(parser, "γιγνεται")
+    @test length(analyzed) == 2
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γιγνεται"
+
+
+    analyzed = parsetoken(parser, "δεικνυσι")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "δεικνυσι"
+
+    analyzed = parsetoken(parser, "δειξεται")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "δειξεται"
+
+    analyzed = parsetoken(parser, "δεδειχε")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "δεδειχε"
+
+    analyzed = parsetoken(parser, "δειχθησεται")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "δειχθησεται"
+
+    analyzed = parsetoken(parser, "γενησοιτο")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γενησοιτο"
+
+    analyzed = parsetoken(parser, "γενοιτο")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γενοιτο"
+
+    analyzed = parsetoken(parser, "γεγονε")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γεγονε"
+
+    analyzed = parsetoken(parser, "γεγενηται")
+    @test length(analyzed) == 2
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γεγενηται"
+
+    analyzed = parsetoken(parser, "γενηθησεται")
+    @test length(analyzed) == 1
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "γενηθησεται"
+
+
+    analyzed = parsetoken(parser, "ποιειται")
+    @test length(analyzed) == 2
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "ποιειται"
+
+
+    analyzed = parsetoken(parser, "πραττεται")
+    @test length(analyzed) == 2
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "πραττεται"
+
+    analyzed = parsetoken(parser, "πεπρακται")
+    @test length(analyzed) == 2
+    @test isa(analyzed[1], Analysis)
+    @test analyzed[1].token == "πεπρακται"
 end
