3b46620f8c8612504a7d983ba2ac7e2f6596e62e
Neel Smith <dnsmith.neel@gmail.com>
2021-08-18 09:00:42 -0400

    Add script to get vocab for a dataset with labelled lexemes

---
+++ scratch/dsvocab.jl
@@ -0,0 +1,41 @@
+# Look at vocab list (lexemes) in a dataset.
+using Kanones
+using HTTP
+using CSV 
+using CitableParserBuilder
+
+
+corevocab = pwd() * "/datasets/core-vocab/"
+coreinfl = pwd() * "/datasets/core-infl/"
+coreds = [coreinfl, corevocab]
+
+ds = Kanones.Dataset(coreds)
+lexlist = map(s -> s.lexid, stemsarray(ds))
+
+lsjurl = "https://raw.githubusercontent.com/neelsmith/Kanones.jl/main/lsj/lsj-lemmata.cex"
+
+lsjdict = CSV.File(HTTP.get(lsjurl).body) |> Dict
+
+
+function labelid(str, dict)
+    if haskey(dict, str)
+        string(str,"_", dict[str])
+    else
+        string(str,"_nolabel")
+    end
+end
+
+
+labelled = map(au -> labelid(abbreviation(au), lsjdict), lexlist )
+
+
+
+stems[1] |> typeof
+
+stems = stemsarray(ds)
+labelled |> length
+
+filter(s -> isa(s,Kanones.NounStem ), stems)
+
+typelist = map(s -> typeof(s), stems) |> unique
+println(typelist)
