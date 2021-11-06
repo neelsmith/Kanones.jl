0e71bf5db1ba98ecdbc0210b28980450f24c4b66
Neel Smith <dnsmith.neel@gmail.com>
2021-11-06 16:26:10 -0400

    Start reorg of finite verb files

--- src/citable_stems/finiteverbs.jl
+++ src/citable_stems/finiteverbs.jl
@@ -40,31 +40,3 @@ function readstemrow(usp::VerbParser, delimited::AbstractString; delimiter = "|"
     VerbStem(stemid,lexid,stem,stemclass)
     # Rule|LexicalEntity|StemClass|Stem|
 end
-
-
-"""Read one row of a rules table for verb tokens and create a `FiniteVerbRule`
-
-$(SIGNATURES)
-"""
-function readrulerow(usp::VerbParser, delimited::AbstractString; delimiter = "|")
-    parts = split(delimited, delimiter)
-    
-    if length(parts) < 8
-        msg = "Invalid syntax for finite verb rule: too few components in $(delimited)"
-        throw(ArgumentError(msg))
-    else
-        ruleid = RuleUrn(parts[1])
-        inflclass = parts[2]
-        ending = parts[3]
-        
-        p = parts[4]
-        n = parts[5]
-        t = parts[6]
-        m = parts[7]
-        v = parts[8]
-
-        FiniteVerbRule(ruleid, inflclass, ending,p,n,t,m,v)
-    end
-
-    # Rule|StemClass|Ending|Person|Number|Tense|Mood|Voice
-end
