a5b481c3b3e13db4f7fb965f685646ec31eacf99
Neel Smith <dnsmith.neel@gmail.com>
2021-11-06 11:06:52 -0400

    Fix copy/paste error

--- src/morphology/pronounmorphology.jl
+++ src/morphology/pronounmorphology.jl
@@ -73,13 +73,13 @@ end
  end
 
 
- """Compose a `FormUrn` for a `PronounForm`.
-
+"""Compose a `FormUrn` for a `PronounForm`.
 $(SIGNATURES)
 """
 function formurn(pronounform::PronounForm)
-    FormUrn(string("morphforms.", PRONOUN,"0",nounform.nnumber,"000",nounform.ngender, nounform.ncase, "00"))
+    FormUrn(string("morphforms.", PRONOUN,"0",pronounform.pnumber,"000",pronounform.pgender,pronounform.pcase,"00"))
 end
+
 
 
 """Compose a PronounForm for a noun form from FST representation of analytical data.
