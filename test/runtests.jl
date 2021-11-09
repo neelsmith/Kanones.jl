4cb524d3da783a710f0ecb3804c7cf211db771dc
Neel Smith <dnsmith.neel@gmail.com>
2021-11-09 08:33:01 -0500

    Fail some tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,23 +5,23 @@ using CitableCorpus, CitableText
 using PolytonicGreek, Orthography
 using Test
 
-# =
+#=
 include("test_morphologyids.jl")
 include("test_morphologycex.jl")
 include("test_morphforms.jl")
 include("test_formurnparsing.jl")
-# =#
+=#
 
-# =
+#=
 include("test_parse.jl")
 include("test_compile.jl")
-# =#
+=#
 
 
 include("test_lexeme.jl")
 
 
-# =
+#=
 include("parsing_output/test_nounsparsing.jl")
 
 include("parsing_output/test_adjsparsing.jl")
@@ -31,25 +31,22 @@ include("parsing_output/test_participlesparsing.jl")
 include("parsing_output/test_pronounsparsing.jl")
 include("parsing_output/test_uninflectedparsing.jl")
 include("parsing_output/test_finiteverbparsing.jl")
-# =#
+=#
 
 
 
-# =
+#=
 include("generate/test_nounsgenerate.jl")
 include("generate/test_uninflectedgenerate.jl")
-# =#
+=#
 
 
-# =
+#=
 include("irregulars/test_irregrules.jl")
-# =#
-
-# =
 include("test_morphproperties.jl")
-# =#
+=#
 
-# = 
+#= 
 include("finiteverbs/test_1.verbsdata.jl")
 include("finiteverbs/test_2.verbsfst.jl") 
 include("finiteverbs/test_3.verbforms.jl")
@@ -90,17 +87,12 @@ include("adjectives/test_3.adjectiveforms.jl")
 include("adjectives/test_4.adjectivestems.jl")
 include("adjectives/test_5.adjectiverules.jl")
 
-
-
 include("uninflected/test_1.uninflecteddata.jl")
 include("uninflected/test_2.uninflectedfst.jl")
 include("uninflected/test_3.uninflectedforms.jl")
 include("uninflected/test_4.uninflectedstems.jl")
 include("uninflected/test_5.uninflectedrules.jl")
-
-
-
-# =#
+=#
 
 # =
 include("accessors/test_nounaccessors.jl")
