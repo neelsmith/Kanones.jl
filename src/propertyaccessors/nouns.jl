e735a1542dbabeabb4208738804110446e143d57
Neel Smith <dnsmith.neel@gmail.com>
2021-11-09 09:26:11 -0500

    Test noun property accessors

--- src/propertyaccessors/nouns.jl
+++ src/propertyaccessors/nouns.jl
@@ -12,8 +12,38 @@ function gmpNumber(noun::GMFNoun)
     noun.nnumber
 end
 
-#gender::GMPGender
+# Regular Stem
 
 function gmpGender(noun::NounStem)    
     noun.gender
+end
+
+# Irregular stem
+
+function gmpGender(noun::IrregularNounStem)    
+    noun.noungender
+end
+
+function gmpCase(noun::IrregularNounStem)    
+    noun.nouncase
+end
+
+function gmpNumber(noun::IrregularNounStem)    
+    noun.nounnumber
+end
+
+
+
+# Rules
+function gmpGender(noun::NounRule)    
+    noun.ngender
+end
+
+function gmpCase(noun::NounRule)    
+    noun.ncase
+end
+
+
+function gmpNumber(noun::NounRule)    
+    noun.nnumber
 end
