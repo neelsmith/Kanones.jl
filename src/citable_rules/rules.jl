1fb494972d8845f5ef2a2b1bd17985b093e5a96f
Neel Smith <dnsmith.neel@gmail.com>
2021-11-09 08:21:30 -0500

    Define generic methods for property accessor functions

--- src/citable_rules/rules.jl
+++ src/citable_rules/rules.jl
@@ -1,7 +1,49 @@
 
 abstract type KanonesRule <: Rule end
 
-function gmpCase(stem::T) where {T <: KanonesRule}
-    @warn "Function gmpCase not implemented for rules of type $(typeof(stem))."
+
+function gmpGender(stem::T) where {T <: KanonesRule}
+    @warn "Function gmpGender not implemented for rules of type $(typeof(stem))."
+    nothing
+end
+
+function gmpCase(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpCase not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpNumber(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpNumber not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+
+function gmpPerson(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpPerson not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpTense(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpTense not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpMood(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpMood not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpVoice(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpVoice not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpDegree(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpDegree not implemented for rules of type $(typeof(rule))."
+    nothing
+end
+
+function gmpUninflected(rule::T) where {T <: KanonesRule}
+    @warn "Function gmpUninflected not implemented for rules of type $(typeof(rule))."
     nothing
 end
