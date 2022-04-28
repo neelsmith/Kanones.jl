89250b67fc9421d4f07f54f5599f4260905fb129
Neel Smith <dnsmith.neel@gmail.com>
2022-04-28 09:47:59 -0400

    Start generating form codes for finite verbs

--- src/generate/generatefiniteverb.jl
+++ src/generate/generatefiniteverb.jl
@@ -20,3 +20,38 @@ function generate(stem::VerbStem, rule::FiniteVerbRule;           ortho::GreekOr
         end
     end
 end
+
+
+
+
+"""Generate list of codes for all noun forms.
+$(SIGNATURES)
+"""
+function verbformcodes()
+    personints = keys(personlabeldict) |> collect |> sort
+    numints = keys(numberlabeldict) |> collect |> sort
+    tenseints = keys(tenselabeldict) |> collect |> sort
+    moodints = keys(moodlabeldict) |> collect |> sort
+    voiceints = keys(voicelabeldict) |> collect |> sort
+    formlist = []
+    for v in voiceints
+        for t in tenseints
+            for m in moodints
+                for n in numints
+                    for p in personints
+                        push!(formlist, "$(FINITEVERB)$(p)$(n)$(t)$(m)$(v)0000")
+                    end
+                end
+            end
+        end
+    end
+    formlist
+end
+
+"""Generate list of all possible noun forms.
+$(SIGNATURES)
+"""
+function verbforms()
+    verbformcodes() .|> gmfFiniteVerb
+end
+
