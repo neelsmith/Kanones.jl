a960281d7eb1774952581cb307e46c4dfd6892aa
Neel Smith <dnsmith.neel@gmail.com>
2022-04-30 10:40:31 -0400

    Test pronoun stems

--- test/stems/test_pronstems.jl
+++ test/stems/test_pronstems.jl
@@ -3,29 +3,27 @@
     pronounio = Kanones.PronounIO("IO for pronouns")
     stem = Kanones.readstemrow(pronounio, delimited)
     @test stem isa PronounStem
-    @test cex(stem) == "nounstems.n8909|Noun stem ἀνθρωπ- (masculine)|ἀνθρωπ|lsj.n8909|os_ou|masculine|recessive"
+    @test cex(stem) == "pronoun.n71882a|Stem ὁ (article: masculine, nominative, singular)|ὁ|lsj.n71882|article|masculine|nominative|singular"
 
-    # code
-    # formurn
 end
 
-#=
-@testset "Test data accessors" begin
-    delimited = "nounstems.n8909|lsj.n8909|ἀνθρωπ|masculine|os_ou|recessive|"
-    nounio = Kanones.NounIO("IO for nouns")
-    stem = Kanones.readstemrow(nounio, delimited)
 
-    expectedurn = StemUrn("nounstems.n8909")
+@testset "Test data accessors" begin
+    delimited = "pronoun.n71882a|lsj.n71882|ὁ|masculine|nominative|singular|article"
+    pronounio = Kanones.PronounIO("IO for pronouns")
+    stem = Kanones.readstemrow(pronounio, delimited)
+    expectedurn = StemUrn("pronoun.n71882a")
     @test urn(stem) == expectedurn
 
-    dict = Dict("nounstems" => "urn:cite2:kanones:nounstems.v1:")
-    expectedcite2 = Cite2Urn("urn:cite2:kanones:nounstems.v1:n8909")
+    dict = Dict("pronoun" => "urn:cite2:kanones:pronoun.v1:")
+    expectedcite2 = Cite2Urn("urn:cite2:kanones:pronoun.v1:n71882a")
     @test urn(stem) == expectedurn
     @test urn(stem, registry = dict) == expectedcite2
 
 
-    @test lexeme(stem) == LexemeUrn("lsj.n8909")
-    @test stemstring(stem) == "ἀνθρωπ"
+    @test lexeme(stem) == LexemeUrn("lsj.n71882")
+    @test stemstring(stem) == "ὁ"
+    @test code(stem) == "9010001100"
+    @test formurn(stem) == FormUrn("forms.9010001100")
     
 end
-=#
