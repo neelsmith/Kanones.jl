5f4f86ece1885d86978e9f6c2df9dff5687a9959
Neel Smith <dnsmith.neel@gmail.com>
2022-04-30 12:35:29 -0400

    Rm old test quarry material

--- quarry-pre-0.4/test/test_dataset.jl
+++
@@ -1,6 +0,0 @@
-@testset ExtendedTestSet "Test building Kanones.Dataset" begin
-    repo = dirname(pwd())
-    infl = joinpath(repo, "datasets", "core-infl")
-    vocab = joinpath(repo, "datasets", "core-vocab")
-    kd = Kanones.Dataset([infl, vocab])
-end
