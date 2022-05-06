00653ac8307c93600d91071d8820c5d8e3de960c
Neel Smith <dnsmith.neel@gmail.com>
2022-05-06 17:21:29 -0400

    Add placeholder files

--- scratch/lysias-nounwork.jl
+++
@@ -1,9 +0,0 @@
-using Kanones
-
-nounsfile = "datasets/lysias-nouns/stems-tables/nouns/lysias.cex"
-Kanones.sortstemfile(nounsfile)
-
-
-
-verbsfile = "datasets/lysias/stems-tables/verbs-simplex/verbs.cex"
-Kanones.sortstemfile(verbsfile)
