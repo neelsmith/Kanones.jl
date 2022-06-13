458f091914b75bb06b7731a4f5ad8b9e0f7f762d
Neel Smith <dnsmith.neel@gmail.com>
2022-06-13 10:49:42 -0400

    Support creating full parses by inflectional type

--- src/parser/perfilebuilder.jl
+++ src/parser/perfilebuilder.jl
@@ -13,24 +13,26 @@ function writecsv(kd::Kanones.FilesDataset, targetdir; msgchunk = 50, threshhold
 end
 
 
-
+"""Compute all possible forms for `rules` and `stems`, and write
+results to delimited-text files in `targetdir`.
+$(SIGNATURES)
+"""
 function writecsv(rules::Vector{Rule}, stems::Vector{Stem}, targetdir; msgchunk = 50, threshhold = 10000, delimiter = ",")
     @info("Write all analyses for rules/stems", length(rules), length(stems))
-
-
-
     analyses = []
     filecount = 0
     totalforms = 0
     
     @debug("Enumerated $(length(stems)) stems")
     for (i, stem) in enumerate(stems)
-        
-        @debug("$(i) Generating for stem", stem)
         if isnothing(stem)
             throw(DomainError("No stem at index $(i)"))
         end
-        append!(analyses, buildparseable(stem, rules, delimiter = delimiter))
+
+        built =  buildparseable(stem, rules, delimiter = delimiter)
+        @debug("$(i) For stem/found", stem, length(built))
+        append!(analyses,built)
+        @debug("Length analyses now", length(analyses))
 
         if length(analyses) >= threshhold
             filecount = filecount + 1
@@ -45,6 +47,7 @@ function writecsv(rules::Vector{Rule}, stems::Vector{Stem}, targetdir; msgchunk 
             @info("Stem $(i) ($(stem)). $(totalforms) forms written to $(filecount) files so far.")
         end
     end
+    filecount = filecount + 1
     f = joinpath(targetdir, "parses$(filecount).csv")
     @info("$(f) (complete)")
     open(f, "w") do io
@@ -59,28 +62,34 @@ function writecsvbytype(kd::Kanones.FilesDataset, targetdir; msgchunk = 50, thre
     verbrules = filter(rulesarray(kd)) do r
         typeof(r) <: Kanones.KanonesVerbalRule
     end
-    infclasses = map(r -> inflectionClass(r), verbrules) |> unique
+    inflclasses = map(r -> inflectionClass(r), verbrules) |> unique
 
-    for inflclass in infclasses
-        @info("Writing data for class ", inflclass)
-        outfile = joinpath(targetdir, string(inflclass))
-        @info("Putting stuff in " , outfile)
-        writecsv(kd, inflclass, targetdir, msgchunk = msgchunk, threshhold = threshhold, delimiter = delimiter)
+    for inflclass in inflclasses
+        writeinfltype(kd, inflclass, targetdir, 
+            msgchunk = msgchunk, threshhold = threshhold, delimiter = delimiter)
     end
-    @info("Done.")
-    
+    @info("Done.")   
 end
 
 
-function writecsv(kd::Kanones.FilesDataset, infclass, targetdir; msgchunk = 50, threshhold = 10000, delimiter = ",")
-    
-    rules = filter(rulesarray(kd)) do r
-       inflectionClass(r) == infclass
+function writeinfltype(kd::Kanones.FilesDataset, inflclass, targetdir; 
+    msgchunk = 50, threshhold = 10000, delimiter = ",")
+
+    outdir = joinpath(targetdir, string(inflclass))
+    if ! isdir(outdir)
+        mkdir(outdir)
     end
+    @info("Putting stuff in " , outdir)
+    rules = filter(rulesarray(kd)) do r
+       inflectionClass(r) == inflclass
+    end   
     stems = filter(stemsarray(kd)) do s
-        inflectionClass(s) == infclass
-     end
-     writecsv(rules, stems, targetdir, msgchunk = msgchunk, threshhold = threshhold, delimiter = delimiter)
+        inflectionClass(s) == inflclass
+    end
+
+    writecsv(rules, stems, outdir, 
+        msgchunk = msgchunk, threshhold = threshhold, delimiter = delimiter)
+     
 end
 
 function buildparses(f, delimitedreader, rules::Vector{Rule}; delimiter = ",")
