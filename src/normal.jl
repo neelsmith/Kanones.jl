296a679bf293f9aefb64f096e52035d325cbebbf
Neel Smith <dnsmith.neel@gmail.com>
2023-05-10 13:40:01 -0400

    Add API docs

--- src/normal.jl
+++ src/normal.jl
@@ -1,5 +1,8 @@
 
-
+"""Normalize string `s` for processing with Kanones
+by making string all lowercase and flipping any grave
+accents to acute.
+"""
 function knormal(s, ortho = literaryGreek())
     lc = nfkc(s) |> lowercase  
     PolytonicGreek.flipaccent(lc, ortho)
