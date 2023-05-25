43dfc6ca8bda24ca06174e58f45e089d3d72715d
Neel Smith <dnsmith.neel@gmail.com>
2023-05-25 08:13:59 -0400

    Add script to build core parser

---
+++ scripts/coreparser.jl
@@ -0,0 +1,38 @@
+using Kanones
+using Dates
+
+
+# Build a parser with demo vocab and manually validated vocab from LSJ.
+function coredata()
+    # 1. demo vocab:
+    lgr = joinpath(pwd(), "datasets", "literarygreek-rules")
+    # 2. manually validated LSJ vocab:
+    lsj = joinpath(pwd(), "datasets", "lsj-vocab")
+    # 3. manually validated NOT in LSJ:
+    extra = joinpath(pwd(), "datasets", "extra")
+    dataset([lgr, lsj, extra])
+end
+
+
+# Compose a single CSV file instantiating a parser from a FilesDataset
+function writedataset(ds::Kanones.FilesDataset)
+    basename = string("literarygreek-core-", today())
+    outputdir = joinpath(pwd(), "scratch", basename)
+    mkpath(outputdir)
+    writecsv(ds, outputdir)outputdir
+
+    lines = ["Token,Lexeme,Form,Stem,Rule"]
+    outfiles = readdir(outputdir)
+    csvfiles = filter(fname -> endswith(fname, ".csv"), outfiles)
+    for csvfile in csvfiles
+        lines = vcat(lines, readlines(joinpath(outputdir, csvfile)))
+    end
+    outfile = joinpath(pwd(), "parsers", basename * ".csv")
+    open(outfile, "w") do io
+        write(io, join(lines, "\n") * "\n")
+    end
+
+    currentfile = joinpath(pwd(), "parsers", "current.csv")
+    cp(outfile, currentfile)
+end
+
