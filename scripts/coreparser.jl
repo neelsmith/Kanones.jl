e49ebac8a8f4672f1b35882ba651116e86020e12
Neel Smith <dnsmith.neel@gmail.com>
2023-06-04 08:01:46 -0400

    Expand coverage

--- scripts/coreparser.jl
+++ scripts/coreparser.jl
@@ -21,13 +21,14 @@ end
 
 # Build a parser with demo vocab and manually validated vocab from LSJ.
 function coredata()
-    # 1. demo vocab:
+    # 1. rules with demo vocab:
     lgr = joinpath(pwd(), "datasets", "literarygreek-rules")
+    ionic = joinpath(pwd(), "datasets", "ionic")
     # 2. manually validated LSJ vocab:
     lsj = joinpath(pwd(), "datasets", "lsj-vocab")
     # 3. manually validated NOT in LSJ:
     extra = joinpath(pwd(), "datasets", "extra")
-    dataset([lgr, lsj, extra])
+    dataset([lgr, ionic, lsj, extra])
 end
 
 
