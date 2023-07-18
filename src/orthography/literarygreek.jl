1ea3dafe7f83f7bda8e2eb8b6eac85cb0bdc6b52
Neel Smith <dnsmith.neel@gmail.com>
2023-07-18 12:31:34 -0400

    Xenophon vocab and classes

--- src/orthography/literarygreek.jl
+++ src/orthography/literarygreek.jl
@@ -1,6 +1,11 @@
 
+"""Mappings of pairs of words written as a single word.
+Includes crasis and attached enclitic forms.
+"""
 crasis = (
     
+    knormal("ἔγωγε") => ["ἔγω", "γε"],
+
     knormal("κἀγὼ") => ["καὶ", "ἐγώ"],
     knormal("κἀκεῖ") => ["καὶ", "ἐκεῖ"],
     knormal("κἀκεῖνος") => ["καὶ", "ἐκεῖνος"],
