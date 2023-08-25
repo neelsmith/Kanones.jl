3363b747be4f833ba33337ffc7cc06ff44d300ae
Neel Smith <dnsmith.neel@gmail.com>
2023-08-25 09:00:35 -0400

    comments

--- test/profiling/test_validateds.jl
+++ test/profiling/test_validateds.jl
@@ -6,10 +6,11 @@
     classmap = Kanones.lexemeclassindex(ds)
     @test classmap[1] isa NamedTuple{(:lexemeid, :inflclass), Tuple{String, String}}
 
+    # all references in stems to inflectional types appear in rules
+
     #=
  all URNs in stems, rules and lexeme tables use registered URNs
  all references in stems to lexemes appear in lexeme collection
- all references in stems to inflectional types appear in rules
  ensure that all URNs for rules, stems and lexemes are unique
  ensure that all references in compound stems to simplex stems exist
     =#
